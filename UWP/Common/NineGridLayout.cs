6707f6265a226ddcb1e4a79026a09313374383db
tlaster <tlaster@outlook.com>
2019-08-25 15:42:41 +0800

    Add padding support for nine grid layout

--- UWP/Common/NineGridLayout.cs
+++ UWP/Common/NineGridLayout.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using Windows.Foundation;
+using Windows.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace WeiPo.Common
 {
     public class NineGridLayout : VirtualizingLayout
     {
+        public static readonly DependencyProperty PaddingProperty = DependencyProperty.Register(
+            nameof(Padding), typeof(double), typeof(NineGridLayout), new PropertyMetadata(8d));
+
+        public double Padding
+        {
+            get => (double) GetValue(PaddingProperty);
+            set => SetValue(PaddingProperty, value);
+        }
+
         protected override Size MeasureOverride(VirtualizingLayoutContext context, Size availableSize)
         {
-            var itemSize = availableSize.Width / 3D;
-            var totalHeight = Math.Ceiling(Convert.ToDouble(context.ItemCount) / 3D) * itemSize;
+            var itemSize = availableSize.Width / 3D - Padding;
+            var rowCount = Math.Ceiling(Convert.ToDouble(context.ItemCount) / 3D);
+            if (rowCount == 0d)
+            {
+                return new Size(availableSize.Width, 0d);
+            }
+            var totalHeight = rowCount * itemSize + (rowCount - 1) * Padding;
             for (var i = 0; i < context.ItemCount; i++)
                 context.GetOrCreateElementAt(i).Measure(new Size(itemSize, itemSize));
 
@@ -20,18 +35,18 @@ namespace WeiPo.Common
         {
             var currentY = 0D;
             var currentX = 0D;
-            var itemSize = finalSize.Width / 3D;
+            var itemSize = finalSize.Width / 3D - Padding;
             for (var i = 0; i < context.ItemCount; i++)
             {
                 var child = context.GetOrCreateElementAt(i);
                 var bounds = new Rect(currentX, currentY, itemSize, itemSize);
                 child.Arrange(bounds);
 
-                currentX += itemSize;
+                currentX += itemSize + Padding;
                 if (currentX >= finalSize.Width)
                 {
                     currentX = 0D;
-                    currentY += itemSize;
+                    currentY += itemSize + Padding;
                 }
             }
 
