3c263aa8f35494f05953e02ee732d414485370cc
Tlaster <tlaster@outlook.com>
2019-10-21 15:59:39 +0800

    Add detail support for album tab

--- UWP/Activities/User/Tab/AlbumTab.xaml.cs
+++ UWP/Activities/User/Tab/AlbumTab.xaml.cs
@@ -1,4 +1,8 @@
-﻿using WeiPo.Services.Models;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+using WeiPo.Common;
+using WeiPo.Services.Models;
+using WeiPo.ViewModels;
 using WeiPo.ViewModels.User.Tab;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -15,6 +19,18 @@ namespace WeiPo.Activities.User.Tab
         protected override AbsTabViewModel CreateViewModel(ProfileData viewModelProfile, Services.Models.Tab tabData)
         {
             return new AlbumTabViewModel(viewModelProfile, tabData);
+        }
+
+        protected override void OnTapped(TappedRoutedEventArgs e)
+        {
+            base.OnTapped(e);
+            if (e.OriginalSource is FrameworkElement element && element.DataContext is PicWall model)
+            {
+                e.Handled = true;
+                
+                Singleton<MessagingCenter>.Instance.Send(this, "image_clicked",
+                    new ImageViewModel(new []{new ImageModel(model.PicMiddle, model.PicBig)}));
+            }
         }
     }
 }
