d2fa1108b09fcdf1aafb1dd134fc9aa6f148d8b6
Tlaster <tlaster@outlook.com>
2019-10-21 16:39:03 +0800

    Add image save support

--- UWP/Activities/ImageActivity.xaml.cs
+++ UWP/Activities/ImageActivity.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Windows.Foundation;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Windows.Foundation;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Flurl.Http;
 using WeiPo.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -41,6 +46,33 @@ namespace WeiPo.Activities
             {
                 var position = e.GetPosition(scrollViewer);
                 scrollViewer.ChangeView(null, null, scrollViewer.ZoomFactor + 1, false);
+            }
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var item = ViewModel.Images[ViewModel.SelectedIndex];
+            var name = Path.GetFileName(item.Source);
+            var picker = new FileSavePicker
+            {
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary, SuggestedFileName = name
+            };
+            picker.FileTypeChoices.Add("Image file", new List<string> { ".jpg", ".png", ".gif" });
+            var file = await picker.PickSaveFileAsync();
+            if (file == null)
+            {
+                return;
+            }
+
+            try
+            {
+                using var fstream = await file.OpenStreamForWriteAsync();
+                using var stream = await item.Source.GetStreamAsync();
+                await stream.CopyToAsync(fstream);
+            }
+            catch
+            {
+                //TODO: show notification
             }
         }
     }
