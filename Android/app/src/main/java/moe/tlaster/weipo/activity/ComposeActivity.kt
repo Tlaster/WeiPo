0ea99b644196cc4bed0410160a6eca703c02795c
tlaster <tlaster@outlook.com>
2019-11-04 23:08:32 +0800

    Update compose activity image list

--- Android/app/src/main/java/moe/tlaster/weipo/activity/ComposeActivity.kt
+++ Android/app/src/main/java/moe/tlaster/weipo/activity/ComposeActivity.kt
@@ -2,17 +2,28 @@ package moe.tlaster.weipo.activity
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Canvas
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_compose.*
+import kotlinx.android.synthetic.main.layout_list.*
 import moe.tlaster.weipo.R
-import moe.tlaster.weipo.common.adapter.AutoAdapter
+import moe.tlaster.weipo.common.adapter.IncrementalLoadingAdapter
 import moe.tlaster.weipo.common.adapter.ItemSelector
 import moe.tlaster.weipo.common.collection.CollectionChangedEventArg
 import moe.tlaster.weipo.common.extensions.viewModel
 import moe.tlaster.weipo.viewmodel.ComposeViewModel
+import java.util.*
+import kotlin.math.abs
+
 
 class ComposeActivity : BaseActivity() {
     private val IMAGE_PICKER_REQUEST_CODE: Int = 3247
@@ -44,13 +55,29 @@ class ComposeActivity : BaseActivity() {
         open_image_picker_button.setOnClickListener {
             openImagePicker()
         }
-        image_picked_list.adapter = AutoAdapter<Uri>(ItemSelector(R.layout.item_image)).apply {
+        image_picked_list.adapter = IncrementalLoadingAdapter<Uri>(ItemSelector(R.layout.item_compose_image)).apply {
             setView<ImageView>(R.id.image) { view, item, _, _ ->
                 view.setImageURI(item)
+            }
+            setView<ImageButton>(R.id.remove_button) { view, item, position, adapter ->
+                view.setOnClickListener {
+                    viewModel.images.remove(item)
+                }
             }
             items = viewModel.images
         }
         viewModel.images.collectionChanged += onImageCollectionChanged
+        ItemTouchHelper(SimpleItemTouchHelperCallback { fromPosition, toPosition ->
+            if (fromPosition < toPosition) {
+                for (i in fromPosition until toPosition) {
+                    Collections.swap(viewModel.images, i, i + 1)
+                }
+            } else {
+                for (i in fromPosition downTo toPosition + 1) {
+                    Collections.swap(viewModel.images, i, i - 1)
+                }
+            }
+        }).attachToRecyclerView(recycler_view)
     }
 
     private fun openImagePicker() {
@@ -83,4 +110,83 @@ class ComposeActivity : BaseActivity() {
             }
         }
     }
+}
+
+class SimpleItemTouchHelperCallback(private val moveCallback: (fromPosition: Int, toPosition: Int) -> Unit) :
+    ItemTouchHelper.Callback() {
+
+    val ALPHA_FULL = 1.0f
+
+    override fun isLongPressDragEnabled(): Boolean {
+        return true
+    }
+
+    override fun isItemViewSwipeEnabled(): Boolean {
+        return false
+    }
+
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        // Set movement flags based on the layout manager
+        return if (recyclerView.layoutManager is GridLayoutManager) {
+            val dragFlags =
+                ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+            val swipeFlags = 0
+            makeMovementFlags(dragFlags, swipeFlags)
+        } else if (recyclerView.layoutManager is LinearLayoutManager && (recyclerView.layoutManager as LinearLayoutManager).orientation == LinearLayout.HORIZONTAL) {
+            val swipeFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val dragFlags = ItemTouchHelper.START or ItemTouchHelper.END
+            makeMovementFlags(dragFlags, swipeFlags)
+        } else {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+            makeMovementFlags(dragFlags, swipeFlags)
+        }
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        source: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        if (source.itemViewType != target.itemViewType) {
+            return false
+        }
+
+        // Notify the adapter of the move
+        moveCallback.invoke(source.adapterPosition, target.adapterPosition)
+        return true
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, i: Int) {
+        // Notify the adapter of the dismissal
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            // Fade out the view as it is swiped out of the parent's bounds
+            val alpha = ALPHA_FULL - abs(dX) / viewHolder.itemView.width.toFloat()
+            viewHolder.itemView.alpha = alpha
+            viewHolder.itemView.translationX = dX
+        } else {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+        }
+    }
+
+
+    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+        super.clearView(recyclerView, viewHolder)
+        viewHolder.itemView.alpha = ALPHA_FULL
+    }
+
 }
