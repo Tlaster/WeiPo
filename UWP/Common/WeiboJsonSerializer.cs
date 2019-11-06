22b1d66cb6f91e667419fcda72adc3b86b7f4dea
Tlaster <tlaster@outlook.com>
2019-11-06 12:23:13 +0800

    Fix WeibosonSerializer null reference issue

--- UWP/Common/WeiboJsonSerializer.cs
+++ UWP/Common/WeiboJsonSerializer.cs
@@ -39,10 +39,12 @@ namespace WeiPo.Common
     public class WeiboJsonSerializer : ISerializer
     {
         private readonly JsonSerializerSettings _settings;
+        private readonly JsonSerializer _defaultSerializer;
 
         public WeiboJsonSerializer(JsonSerializerSettings settings)
         {
             _settings = settings;
+            _defaultSerializer = JsonSerializer.CreateDefault(_settings);
         }
 
         public string Serialize(object obj)
@@ -54,16 +56,20 @@ namespace WeiPo.Common
         {
             var obj = JsonConvert.DeserializeObject<JToken>(s, _settings);
             CheckIfError(obj);
-            return obj.ToObject<T>(JsonSerializer.CreateDefault(_settings));
+            return obj.ToObject<T>(_defaultSerializer);
         }
 
         public T Deserialize<T>(Stream stream)
         {
             using var streamReader = new StreamReader(stream);
             using var jsonTextReader = new JsonTextReader(streamReader);
-            var obj = JsonSerializer.CreateDefault(_settings).Deserialize<JToken>(jsonTextReader);
+            var obj = _defaultSerializer.Deserialize<JToken>(jsonTextReader);
+            if (obj == null)
+            {
+                return default;
+            }
             CheckIfError(obj);
-            return obj.ToObject<T>(JsonSerializer.CreateDefault(_settings));
+            return obj.ToObject<T>(_defaultSerializer);
         }
 
         private static void CheckIfError(JToken obj)
