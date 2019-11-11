0df972522e46e228bca94856df9efdc8a0bc4625
Tlaster <tlaster@outlook.com>
2019-11-11 17:23:17 +0800

    Fix #14

--- Android/app/src/main/java/moe/tlaster/weipo/activity/LoginActivity.kt
+++ Android/app/src/main/java/moe/tlaster/weipo/activity/LoginActivity.kt
@@ -17,19 +17,22 @@ class LoginActivity : BaseActivity() {
         get() = R.layout.activity_login
 
     @ImplicitReflectionSerializer
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         webView.source = "https://m.weibo.cn/login?backURL=https%253A%252F%252Fm.weibo.cn%252F"
         webView.onPageStarted {
             if (!isFinishing) {
-                val cookies = CookieManager.getInstance().getCookie(it).split(';').map {
-                    val res = it.split('=')
-                    res[0].trim() to res[1].trim()
-                }.toMap()
-                if (cookies.containsKey("MLOGIN") && cookies["MLOGIN"] == "1") {
-                    Settings.set("user_cookie", Json.nonstrict.stringify(cookies))
-                    openActivity<HomeActivity>()
-                    finish()
+                CookieManager.getInstance().getCookie(it)?.let { cookies ->
+                    cookies.split(';').map {
+                        val res = it.split('=')
+                        res[0].trim() to res[1].trim()
+                    }.toMap()
+                }?.let { cookies ->
+                    if (cookies.containsKey("MLOGIN") && cookies["MLOGIN"] == "1") {
+                        Settings.set("user_cookie", Json.nonstrict.stringify(cookies))
+                        openActivity<HomeActivity>()
+                        finish()
+                    }
                 }
             }
         }
