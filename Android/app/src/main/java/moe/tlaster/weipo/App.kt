241c3f095c3db4459e3865c1775c36339dd9d59f
Tlaster <tlaster@outlook.com>
2019-12-13 18:48:49 +0800

    Add notification support

--- Android/app/src/main/java/moe/tlaster/weipo/App.kt
+++ Android/app/src/main/java/moe/tlaster/weipo/App.kt
@@ -1,7 +1,10 @@
 package moe.tlaster.weipo
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.piasy.biv.BigImageViewer
 import com.github.piasy.biv.loader.glide.GlideImageLoader
@@ -23,5 +26,22 @@ class App : Application() {
             addRequestInterceptor(CookieRequestInterceptor)
         }
         BigImageViewer.initialize(GlideImageLoader.with(appContext))
+        createNotificationChannel()
     }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val channelId = getString(R.string.channel_id)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 }
--- Android/app/src/main/java/moe/tlaster/weipo/activity/HomeActivity.kt
+++ Android/app/src/main/java/moe/tlaster/weipo/activity/HomeActivity.kt
@@ -2,12 +2,15 @@ package moe.tlaster.weipo.activity
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_home.*
 import moe.tlaster.weipo.R
 import moe.tlaster.weipo.common.extensions.openActivity
+import moe.tlaster.weipo.services.models.UnreadData
 import moe.tlaster.weipo.viewmodel.ComposeViewModel
 import moe.tlaster.weipo.viewmodel.NotificationViewModel
 
@@ -35,9 +38,12 @@ class HomeActivity : BaseActivity() {
         }
         notificationViewModel.unread.observe(this, Observer {
             bottom_navigation_view?.let { bottomNavigationView ->
-                val total = (it.mentionStatus ?: 0) + (it.mentionCmt ?: 0) + (it.cmt ?: 0) + (it.attitude ?: 0) + (it.dm ?: 0)
+                val total =
+                    (it.mentionStatus ?: 0) + (it.mentionCmt ?: 0) + (it.cmt ?: 0) + (it.attitude
+                        ?: 0) + (it.dm ?: 0)
                 if (total > 0) {
-                    bottomNavigationView.getOrCreateBadge(R.id.navigation_notification).number = total.toInt()
+                    bottomNavigationView.getOrCreateBadge(R.id.navigation_notification).number =
+                        total.toInt()
                 } else {
                     bottomNavigationView.removeBadge(R.id.navigation_notification)
                 }
@@ -45,6 +51,66 @@ class HomeActivity : BaseActivity() {
             navigation_view?.let { navigationView ->
                 //TODO: badge
             }
+            sendNotification(it)
         })
+    }
+
+    private fun sendNotification(it: UnreadData) {
+        if ((it.mentionStatus ?: 0) > 0) {
+            buildAndSendNotification(
+                getString(R.string.notification_mention_status).format((it.mentionStatus ?: 0)),
+                R.integer.notification_mention_status
+            )
+        } else {
+            cancelNotification(R.integer.notification_mention_status)
+        }
+        if ((it.mentionCmt ?: 0) > 0) {
+            buildAndSendNotification(
+                getString(R.string.notification_mention_cmt).format((it.mentionCmt ?: 0)),
+                R.integer.notification_mention_cmt
+            )
+        } else {
+            cancelNotification(R.integer.notification_mention_cmt)
+        }
+        if ((it.cmt ?: 0) > 0) {
+            buildAndSendNotification(
+                getString(R.string.notification_cmt).format((it.cmt ?: 0)),
+                R.integer.notification_cmt
+            )
+        } else {
+            cancelNotification(R.integer.notification_cmt)
+        }
+        if ((it.dm ?: 0) > 0) {
+            buildAndSendNotification(
+                getString(R.string.notification_dm).format((it.dm ?: 0)),
+                R.integer.notification_dm
+            )
+        } else {
+            cancelNotification(R.integer.notification_dm)
+        }
+        if ((it.follower ?: 0) > 0) {
+            buildAndSendNotification(
+                getString(R.string.notification_follower).format((it.follower ?: 0)),
+                R.integer.notification_follower
+            )
+        } else {
+            cancelNotification(R.integer.notification_follower)
+        }
+    }
+
+    private fun cancelNotification(notificationId: Int) {
+        with(NotificationManagerCompat.from(this)) {
+            cancel(notificationId)
+        }
+    }
+
+    private fun buildAndSendNotification(title: String, notificationId: Int) {
+        val builder = NotificationCompat.Builder(this, getString(R.string.channel_id))
+            .setAutoCancel(true)
+            .setSmallIcon(R.drawable.ic_notification_icon)
+            .setContentTitle(title)
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
     }
 }
